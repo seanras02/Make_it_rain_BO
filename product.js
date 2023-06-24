eba3adcc8aca8657097fe9742f10bbf7f25e750d
Sean <34967@ma-web.nl>
2023-06-24 05:37:37 +0200

    js testing, positioning, css changes

--- product.js
+++ product.js
@@ -6,20 +6,7 @@ let cartMessageValue = document.getElementsByClassName("shoppingCart__message")[
 const shoppingModal = document.getElementById("js--shoppingModal");
 let modalIOpen = false;
 
-let sf = 0; 
-let tw   = 0;
-let kq  = 0;
 let bu  = 0;
-let di  = 0;
-let ab  = 0;
-let es  = 0;
-let jj  = 0;
-let jk  = 0;
-let na  = 0;
-let tw_  = 0;
-let pu  = 0;
-let th  = 0;
-let ge  = 0;
 let cd  = 0;
 let totalPrice = 0;
 
@@ -50,8 +37,6 @@ for(let i = 0; i < addToCartButtons.length; i++){
     }
 }
 
-document.getElementById("js--totaal--prijs").textContent = "€" + totalPrice;
-
 const checkOutButton = document.getElementById("js--checkOutButton");
 const checkOutWindow = document.getElementById("js--checkOutWindow");
 let checkOutIsOpen = false;
@@ -68,19 +53,7 @@ checkOutButton.onclick = function(){
         document.getElementById("js--amount-bu").innerHTML = bu + "x";
         //important
 
-        document.getElementById("js--amount-sf").innerHTML = sf + "x";
-        document.getElementById("js--amount-tw").innerHTML = tw + "x";
-        document.getElementById("js--amount-kq").innerHTML = kq + "x";
-        document.getElementById("js--amount-tw_").innerHTML = tw_ + "x";
-        document.getElementById("js--amount-di").innerHTML = di + "x";
-        document.getElementById("js--amount-ab").innerHTML = ab + "x";
-        document.getElementById("js--amount-es").innerHTML = es + "x";
-        document.getElementById("js--amount-ge").innerHTML = ge + "x";
-        document.getElementById("js--amount-jj").innerHTML = jj + "x";
-        document.getElementById("js--amount-jk").innerHTML = jk + "x";
-        document.getElementById("js--amount-na").innerHTML = na + "x";
-        document.getElementById("js--amount-pu").innerHTML = pu + "x";
-        document.getElementById("js--amount-th").innerHTML = th + "x";
+        document.getElementById("js--totaal--prijs").textContent = "€" + totalPrice;
         return;
     }
         document.querySelector("main .parts").style.display = "flex";
@@ -88,6 +61,8 @@ checkOutButton.onclick = function(){
         checkOutWindow.style.display = "none";
         checkOutIsOpen = false;
 }
+
+
 
 
 //SEE MORE
